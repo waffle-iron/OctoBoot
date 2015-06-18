4675117b999dd8ac9394828a8646eb23675c9001
Ronan Drouglazet <ronan.drouglazet@ebuzzing.com>
2015-06-19 01:34:20 +0200

    remove Login.hbs

--- src/front/model/UI.ts
+++ src/front/model/UI.ts
@@ -6,13 +6,11 @@ module OctoBoot.model {
         public static HB_REPOS: string = 'Repos.hbs';
         public static HB_SIDEBAR: string = 'Sidebar.hbs';
         public static HB_NEW_REPO: string = 'NewRepo.hbs';
+        public static HB_ALERT: string = 'Alert.hbs';
+        
         public static SIDEBAR_LAUNCH_BT: string = '.launch.button';
 
-        public static HB_LOGIN_MODAL: string = 'Login.hbs';
-        public static LOGIN_MODAL_CONNECT: string = '#login .button';
         public static LOGIN_TITLE: string = 'OctoBoot <-> GitHub';
         public static LOGIN_BODY: string = 'Sorry, you have to be connected with your GitHub account in order to use OctoBoot. Click on the connect button OR if it\'s your first time allow OctoBoot access on your GitHub account';
-
-        public static HB_ALERT: string = 'Alert.hbs';
     }
 }
