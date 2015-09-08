0fb7cd560e53f7b327dab13af4bd1afd0a54fb9e
Ronan Drouglazet <ronan.drouglazet@ebuzzing.com>
2015-09-09 00:04:00 +0200

    Force copy if file exist

--- src/back/modules/copy.js
+++ src/back/modules/copy.js
@@ -5,7 +5,7 @@ exports.init = function(dirP, dirT, sockets) {
         var template = data.template.replace(/\[|\]|\s/ig, "\\$&");
         var dest = dirP + data.sid + "/" + data.project + (data.file === "index" ? "/" : "/" + data.file + "/");
         var src = dirT + "/" + template + (data.file === "index" ? "/*" : "");
-        cp.exec("cp -r " + src + " " + dest, function(error, stdout, stderr) {
+        cp.exec("cp -rf " + src + " " + dest, function(error, stdout, stderr) {
             sockets[data.sid].s.emit("cp", !error);
             sockets[data.sid].s.emit("save_available");
         });
