230d56fa901b8835463c7e5070ecf54129c8b25a
Ronan Drouglazet <ronan.drouglazet@ebuzzing.com>
2016-02-12 23:30:41 +0100

    change 'project' on 'website' for user

--- src/front/model/UI.ts
+++ src/front/model/UI.ts
@@ -22,9 +22,9 @@ module OctoBoot.model {
         public static LOGIN_TITLE: string = 'OctoBoot <-> GitHub';
         public static LOGIN_BODY: string = 'Sorry, you have to be connected with your GitHub account in order to use OctoBoot. Click on the connect button OR if it\'s your first time allow OctoBoot access on your GitHub account';
 
-        public static REPO_ALERT_NEW_BODY: string = 'Cool ! It\'s a greet idea ! Give me a name for your new project please:';
-        public static REPO_ALERT_CONVERT_TITLE: string = 'Not a OctoBoot project';
-        public static REPO_ALERT_CONVERT_BODY: string = 'Sorry, it seems that your project are not a OctoBoot project, did you want to convert it ?';
+        public static REPO_ALERT_NEW_BODY: string = 'Cool ! It\'s a greet idea ! Give me a name for your new website please:';
+        public static REPO_ALERT_CONVERT_TITLE: string = 'Not a OctoBoot website';
+        public static REPO_ALERT_CONVERT_BODY: string = 'Sorry, it seems that your website are not a OctoBoot website, did you want to convert it ?';
 
         public static PUBLISH_ALERT_TITLE: string = 'Publish your website online';
         public static PUBLISH_ALERT_BODY: string = 'Are you sure ? This will update your website online with your current working version';
@@ -32,7 +32,7 @@ module OctoBoot.model {
         public static DELETE_FILE_ALERT_TITLE: string = 'Delete file';
         public static DELETE_FILE_ALERT_BODY: string = 'you are going to remove [FILE] - Are you sure about this ?';
 
-        public static DELETE_PROJECT_ALERT_TITLE: string = 'Delete project';
-        public static DELETE_PROJECT_ALERT_BODY: string = 'Delete [PROJECT] - Are you sure about this ? please type the project name to confirm the deletion';
+        public static DELETE_PROJECT_ALERT_TITLE: string = 'Delete website';
+        public static DELETE_PROJECT_ALERT_BODY: string = 'Delete [PROJECT] - Are you sure about this ? please type the website name to confirm the deletion';
     }
 }
