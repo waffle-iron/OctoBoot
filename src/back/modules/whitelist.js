3d422c7c8de9fe481e4d44f9c1615d31ce4844f3
Ronan Drouglazet <ronan.drouglazet@ebuzzing.com>
2016-07-25 17:39:19 +0200

    fix whitelist for vps

--- src/back/modules/whitelist.js
+++ src/back/modules/whitelist.js
@@ -1,19 +1,13 @@
-var fs = require("fs"),
-pa = require("path")
+var fs = require("fs")
 
 module.exports = (req, res) => {
-    var file = pa.resolve("config/whitelist." + req.params.for + ".json"), j = {}
-    if (file) {
-        fs.readFile(file, (err, data) => {
-            if (!err && data) {
-                try { j = JSON.parse(data) } catch (e) {}
-                res.sendStatus(j[req.params.from] ? 200 : 401)
-            } else {
-                res.sendStatus(401)
-            }
-        })
-    } else {
-        res.sendStatus(401)
-    }
-
+    var file = __dirname + "/../../../config/whitelist." + req.params.for + ".json", j = {}
+    fs.readFile(file, (err, data) => {
+        if (!err && data) {
+            try { j = JSON.parse(data) } catch (e) {}
+            res.sendStatus(j[req.params.from] ? 200 : 401)
+        } else {
+            res.sendStatus(401)
+        }
+    })
 }
